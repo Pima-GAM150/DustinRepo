7cb390b9c52c00b43d1e7ab54b8ed42c80ecdff4
Dustman <dreed12@mail.pima.edu>
2018-09-05 21:16:50 -0700

    starting to make something happen in the classic scene

---
+++ Assets/classic/scripts/RotateAround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    Vector3 rotateLocation;
+    void Start ()
+    {
+        rotateLocation = target.transform.position;
+    }
+    void Update ()
+    {
+        this.transform.RotateAround(rotateLocation, Vector3.up, speed * Time.deltaTime);
+	}
+}
